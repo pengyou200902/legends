899f72cdb9982daaaf8a6755681878159b4a9edf
You Peng <youpeng@bu.edu>
2021-11-07 15:26:10 -0500

    fix generate

--- LegendWorld.java
+++ LegendWorld.java
@@ -14,38 +14,62 @@ public class LegendWorld extends World {
     protected int teamY;
     protected String teamTile;
 
-    /**
-     * Generate the world
-     */
+    public LegendWorld() {
+        super();
+        this.teamX = 0;
+        this.teamY = 0;
+        this.teamTile = " ";
+    }
+
+    public LegendWorld(int width, int height) {
+        super(width, height);
+        this.teamX = (int) Math.round(Math.random() * this.width);
+        this.teamY = (int) Math.round(Math.random() * this.height);
+        this.teamTile = " ";
+    }
+
+    // generate the world
     @Override
     public void generate() {
         int total = this.width * this.height;
         int inaccessible = (int) Math.round(total * 0.2);
         int market = (int) Math.round(total * 0.3);
+
         for (int i = 0; i < this.width; i++) {
             for (int j = 0; j < this.height; j++) {
                 double randNum = Math.random();
+
                 if (randNum < 0.2 && inaccessible > 0) {
                     this.map[i][j] = INACCESSIBLE_TILE;
                     inaccessible -= 1;
-                } else if (inaccessible > 0 && randNum < 0.6 && market > 0) {
+                }
+                else if ((inaccessible > 0 && randNum < 0.6 || randNum < 0.5) && market > 0) {
                     this.map[i][j] = MARKET_TILE;
                     market -= 1;
-                } else if (randNum < 0.5 && market > 0) {
-                    this.map[i][j] = MARKET_TILE;
-                    market -= 1;
-                } else {
+                }
+                else {
                     this.map[i][j] = COMMON_TILE;
                 }
             }
         }
         // Set Team pos
         this.setTile(this.teamX, this.teamY, TEAM_TILE);
+        fixSurroundTeam();
     }
 
-    /**
-     * Update the position of the team
-     */
+    private void fixSurroundTeam() {
+        if (teamX < height - 1 && teamX > 0 && teamY > 0 && teamY < width - 1) {
+                map[teamX - 1][teamY] = COMMON_TILE;
+        }
+        else if (teamX == 0 || teamX == height - 1) {
+            map[teamX == 0 ? 1 : height - 2][teamY] = COMMON_TILE;
+        }
+        else if (teamY == 0 || teamY == width - 1) {
+            map[teamX][teamY == 0 ? 1 : height - 2] = COMMON_TILE;
+        }
+    }
+
+    // update the position of the team
     public void setTeamLocation(int x, int y) {
         updateMap(x, y);
         this.teamX = x;
@@ -53,9 +77,7 @@ public class LegendWorld extends World {
 
     }
 
-    /**
-     * Update the map signs
-     */
+    // update the map signs
     public void updateMap(int x, int y) {
         // refill old tile
         this.setTile(this.teamX, this.teamY, this.teamTile);
@@ -65,11 +87,9 @@ public class LegendWorld extends World {
         this.setTile(x, y, TEAM_TILE);
     }
 
-    /**
-     * Check tile is inaccessible
-     */
+    // check tile is inaccessible
     public boolean isInaccessible(int x, int y) {
-        return this.map[x][y].equals(INACCESSIBLE_TILE);
+        return x < 0 || x >= height || y < 0 || y >= width || map[x][y].equals(INACCESSIBLE_TILE);
     }
 
 }
